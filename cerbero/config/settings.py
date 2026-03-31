4713531c896020627be5f2a666f6ed8b913a73d8
wjpv8 <Wils.jhos87@gmail.com>
2026-03-31 00:54:01 -0500

    Agregar django-extensions o eliminarlo2

--- cerbero/config/settings.py
+++ cerbero/config/settings.py
@@ -69,7 +69,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
     'users',
     'projects',
     'core',
